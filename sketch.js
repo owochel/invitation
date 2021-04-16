f2c8947f6aac533a34379477156520bb680a6fe3
Shuang Cai <51350490+owochel@users.noreply.github.com>
2021-04-16 07:35:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,10 +6,11 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(640, 480, WEBGL);
+  createCanvas(1280, 960, WEBGL);
   capture = createCapture(VIDEO);
   capture.size(640, 480);
   capture.hide();
+  noStroke();
 
   fill(0,0,255);
   textSize(20);
@@ -20,16 +21,19 @@ function setup() {
 
 
 function draw() {
+
   background(255);
-  rect(330, 0, 55, 55);
-  image(capture, -320, -240, 640, 480);
+  image(capture, -640, -480, 640, 480);
+  //filter(INVERT);
+  image(capture,0, -480, 640, 480);
   let date = "Apr.25th - May.1st"
   let loc = "Fisher Studio Art Building Lobby, Bard College, NY"
   let title=".. Title .."
-  //filter(INVERT);
+
+  
   if (mouseX > 320 && mouseY<200) {
     push();
-    fill(0);
+    fill(0,0,0,90);
     translate(100, -100, 60);
     rotateY(PI);
     box(textWidth(date)+30, 160, 10);
@@ -48,7 +52,7 @@ function draw() {
     rotateX((time+mouseX) / 1000);
     rotateZ((time+mouseY) / 1234);
     rotateY(1)
-    fill(0);
+    fill(0,0,0,90);
     translate(0, 50, -10)
     box(textWidth(date)+10, 150, 10)
     fill(0,0,255)
@@ -65,9 +69,5 @@ function draw() {
     text(title,-110,30,textWidth(date),150)
     pop();
   }
-
-}
-
-function stopText() {
 
 }
